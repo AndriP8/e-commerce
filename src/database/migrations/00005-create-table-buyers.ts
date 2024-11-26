fd51155e180be789faa4965356ce467329dac7dd
Andri Purnomo <andri.adrp@gmail.com>
2024-11-26 23:32:01 +0700

    chore: add migration to create table buyers

---
+++ src/database/migrations/00005-create-table-buyers.ts
@@ -0,0 +1,25 @@
+import { Kysely } from "kysely";
+import { DB } from "kysely-codegen";
+
+import {
+  PRIMARY_KEY_COLUMN,
+  SOFT_DELETE_COLUMN,
+  TIMESTAMPS_COLUMN,
+} from "../utils/common-column";
+
+export async function up(db: Kysely<DB>): Promise<void> {
+  await db.schema
+    .createTable("buyers")
+    .$call(PRIMARY_KEY_COLUMN)
+    .addColumn("name", "varchar", (col) => col.notNull().unique())
+    .addColumn("email", "varchar", (col) => col.notNull().unique())
+    .addColumn("password", "varchar", (col) => col.notNull())
+    .addColumn("address", "jsonb")
+    .$call(TIMESTAMPS_COLUMN)
+    .$call(SOFT_DELETE_COLUMN)
+    .execute();
+}
+
+export async function down(db: Kysely<DB>): Promise<void> {
+  await db.schema.dropTable("buyers").execute();
+}
