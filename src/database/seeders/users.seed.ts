1e308e472b52266ec7c74501ae7a914c44a04d2d
Andri Purnomo <andri.adrp@gmail.com>
2024-12-16 21:50:36 +0700

    fix: update user seeder and delete register api

--- src/database/seeders/users.seed.ts
+++ src/database/seeders/users.seed.ts
@@ -3,13 +3,13 @@ import { DB } from "kysely-codegen";
 
 import { hashPassword } from "../../lib/helpers/password";
 
-export const userSeeder = async (db: Kysely<DB>) => {
+export default async function userSeeder(db: Kysely<DB>) {
   return db
     .insertInto("users")
     .values({
-      email: "admin@example.com",
-      password: await hashPassword("admin123,"),
-      name: "admin",
+      email: process.env.ADMIN_EMAIL || "",
+      password: await hashPassword(process.env.ADMIN_PASSWORD || ""),
+      name: process.env.ADMIN_NAME || "",
     })
     .executeTakeFirst();
-};
+}
