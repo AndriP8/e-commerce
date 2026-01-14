1545be40c7f3043cdf7743688f7129b058ebc218
Andri Purnomo <andri.adrp@gmail.com>
2026-01-14 17:51:10 +0700

    fix: robots file to disable ai user agent

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,11 +4,25 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/api/", "/checkout/", "/order-confirmation/"],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/checkout/", "/order-confirmation/"],
+      },
+      {
+        userAgent: [
+          "GPTBot",
+          "CCBot",
+          "Google-Extended",
+          "Anthropic-AI",
+          "Claude-Web",
+          "Omgilibot",
+          "FacebookBot",
+        ],
+        disallow: "/",
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
